172e4fed8007b2571a95e32a5ffa7611cff7ffda
Nuno Teixeira <nuno.miguel.t@gmail.com>
2014-03-05 13:06:31 +0000

    Create unicode.php

---
+++ unicode.php
@@ -0,0 +1,7 @@
+<?php
+
+$lang = array(
+
+"stuff" => "têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü —— têst str@ngæ chá®ã©têrs ±∑ü");
+
+?>
